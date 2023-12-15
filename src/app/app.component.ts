3a13429af99bd1167a4703043bdeb8c41b8b4f09
patelvaibhavd <patelvaibhav27@gmail.com>
2023-12-15 15:37:44 +0530

    configured auth service with local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from './loading/loading.service';
 import { MessagesService } from './messages/messages.service';
+import { AuthStoreService } from './services/auth.store.service';
 
 
 @Component({
@@ -10,17 +11,15 @@ import { MessagesService } from './messages/messages.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {
-
-  }
+  constructor(
+    public authStoreService: AuthStoreService
+  ) { }
 
   ngOnInit() {
-
-
   }
 
   logout() {
-
+    this.authStoreService.logOut();
   }
 
 }
